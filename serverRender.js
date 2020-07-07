fc1d12d1768eef4a2b827726c8e48701a35a67e9
dfgupta <deepak.c.gupta@oracle.com>
2020-07-07 23:44:14 +0530

    6.6 Server-side routing for a contest

--- serverRender.js
+++ serverRender.js
@@ -6,12 +6,39 @@ import App from './src/components/App';
 import axios from 'axios';
 import config from './config';
 
-const serverRender = () =>
-  axios.get(`${config.serverUrl}/api/contests`)
+
+const getApiUrl = contestId => {
+  if(contestId) {
+    return `${config.serverUrl}/api/contests/${contestId}`;
+  }
+
+  return `${config.serverUrl}/api/contests`;
+};
+
+const getInitialData = (contestId, apiData) => {
+  if(contestId) {
+    return  {
+      currentContestId: apiData.id,
+      contests: {
+        [apiData.id]: apiData
+      }
+    };
+  }
+  
+  return {
+    contests: apiData.contests
+  };
+};
+
+const serverRender = (contestId) =>
+  axios.get(getApiUrl(contestId))
     .then(resp => {
+      const initialData = getInitialData(contestId, resp.data);
       return {
-        initialMarkup: ReactDOMServer.renderToString(<App initialContests={resp.data.contests} />),
-        initialData: resp.data
+        initialMarkup: ReactDOMServer.renderToString(
+          <App initialData={initialData} />
+        ),
+        initialData
       };
     });
 
