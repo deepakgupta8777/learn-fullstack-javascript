2df95bd46396913b395ecb48100ca0b505ca4237
dfgupta <deepak.c.gupta@oracle.com>
2020-07-18 15:54:29 +0530

    7.1 Reeading Data from MongoDB

--- config.js
+++ config.js
@@ -1,6 +1,13 @@
 var env = process.env;
 
+export const logStars = function(message) {
+  console.info('**********');
+  console.info(message);
+  console.info('**********');
+};
+
 export default {
+  mongodbUri: 'mongodb://localhost:27017/test',
   port: env.PORT || 3000,
   host: env.HOST || '0.0.0.0',
   get serverUrl() {
